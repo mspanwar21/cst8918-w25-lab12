e8ef92e51454b759e0b221b679ff8c67cd678c2d
Mohit Singh Panwar <panw0011@algonquinlive.com>
2025-07-23 17:40:13 -0400

    Refactor Terraform configuration for improved readability

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
 
   backend "azurerm" {
-    resource_group_name   = "panw0011-githubactions-rg"
-    storage_account_name  = "panw0011githubactions"
-    container_name        = "tfstate"
-    key                   = "prod.app.tfstate"
+    resource_group_name  = "panw0011-githubactions-rg"
+    storage_account_name = "panw0011githubactions"
+    container_name       = "tfstate"
+    key                  = "prod.app.tfstate"
   }
 }
 

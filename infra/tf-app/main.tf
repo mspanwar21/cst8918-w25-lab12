35ec040eb87178de63f5daed9b619e9a3f8e6432
Mohit Singh Panwar <panw0011@algonquinlive.com>
2025-07-23 17:34:38 -0400

    Add Terraform configuration for Azure resource group and backend setup

---
+++ infra/tf-app/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "app_rg" {
+  name     = "panw0011-a12-rg"
+  location = "Canada Central"
+}
---
+++ infra/tf-app/terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "panw0011-githubactions-rg"
+    storage_account_name  = "panw0011githubactions"
+    container_name        = "tfstate"
+    key                   = "prod.app.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
